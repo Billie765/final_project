9ec069ae0d3d10e02fe810cc292fee78b0cabce0
starchenko <igor.starchenko@gmail.com>
2021-11-21 19:31:59 +0200

    Added util files

---
+++ src/test/java/api/util/Kharkiv.java
@@ -0,0 +1,11 @@
+package api.util;
+
+public class Kharkiv {
+    public static String city = "Kharkiv";
+    public static Double lon = 36.25;
+    public static int lat = 50;
+    public static String country = "UA";
+    public static int timezone = 7200;
+    public static int cityId = 706483;
+
+}
